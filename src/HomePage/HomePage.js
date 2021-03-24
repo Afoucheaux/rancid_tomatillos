c673cc3fc8afb396db349f90435b88aca18de53c
Elizabeth Hahn <elizhahn87@gmail.com>
2021-03-24 17:17:06 -0600

    Add classes and styling to HomePage component

--- src/HomePage/HomePage.js
+++ src/HomePage/HomePage.js
@@ -1,21 +1,32 @@
-import React, { Component }from "react";
+import React, { Component } from "react";
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
+import MovieCard from "../MovieCard/MovieCard";
 import movieData from "../data/movies.js";
+import "./HomePage.css";
 
 
 class HomePage extends Component {
   constructor() {
     super()
     this.state= {
-      movies: [],
+      movies: [  {
+        id: 501953,
+      posterImage: "https://image.tmdb.org/t/p/original//2wXrBtfrvwMWE1i3iHjKjoRZjYk.jpg",
+      backdropImage: "https://image.tmdb.org/t/p/original//oeaLQKoPFQxvhEz3yyR1QuestXG.jpg",
+      title: "Eternal Beauty",
+      averageRating: 3,
+      releaseDate: "2020-08-21"
+    }],
     }
   }
   render() {
     return (
        <>
         <Header/>
-        <main>Movies Go Here</main>
+        <main>
+          <MovieCard movieInfo={this.state.movies[0]}/>
+        </main>
         <Footer/>
        </>  
     )
