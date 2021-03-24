4d380de459acbe7e3c43d3eed64a230c7d78dcff
Elizabeth Hahn <elizhahn87@gmail.com>
2021-03-24 15:36:47 -0600

    Add Header component files and edit HomePage to use Header

---
+++ src/Header/Header.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <h1 className="header-title">Rotton Tomatillos</h1>
+      <button className="home-btn">Home</button>
+    </header>
+  )
+}
+
+export default Header;
--- src/HomePage/HomePage.js
+++ src/HomePage/HomePage.js
@@ -1,12 +1,23 @@
 import React, { Component }from "react";
+import Header from "../Header/Header";
+import movieData from "../data/movies.js";
 
 
 class HomePage extends Component {
   constructor() {
     super()
     this.state= {
-      
+      movies: [],
     }
+  }
+  render() {
+    return (
+       <>
+        <Header/>
+        <main>Movies Go Here</main>
+        <footer>Our contact info here</footer>
+       </>  
+    )
   }
 }
 
