0e0340a8246b18bbe72b5cd37624e82af6ae4c53
Elizabeth Hahn <elizhahn87@gmail.com>
2021-03-24 16:01:21 -0600

    Add styling to header and body elements

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,10 +1,13 @@
 import React, { Component } from "react";
+import "./Header.css";
 
 const Header = () => {
   return (
     <header className="header">
       <h1 className="header-title">Rotton Tomatillos</h1>
-      <button className="home-btn">Home</button>
+      <div>
+        <button className="home-btn">Home</button>
+      </div>
     </header>
   )
 }
