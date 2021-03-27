bcc53f44941dc63153d08c99a7c468b658107605
Elizabeth Hahn <elizhahn87@gmail.com>
2021-03-26 21:11:31 -0600

    Refactor App to connect to Api

--- src/App/App.js
+++ src/App/App.js
@@ -7,12 +7,22 @@ class App extends Component {
   constructor() {
     super()
     this.state = {
-      clickedMovieId: ""
+      clickedMovieId: "",
+      singleMovie: {},
+      trailer: {}
     }
   }
 
   onMovieClick = (id) => {
-    this.setState({ clickedMovieId: id })
+    const apiData = [fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${id}`), fetch(`https://rancid-tomatillos.herokuapp.com/api/v2/movies/${id}/videos`)];
+
+    Promise.all(apiData)
+    .then(responses => Promise.all(responses.map(response => response.json())))
+    .then(data => {
+      const [singleMovie, trailer] = data;
+        this.setState({clickedMovieId: id, singleMovie: singleMovie.movie, trailer: trailer.videos[0]})
+    })
+    .catch(error => console.log(error))
   }
   onHomeClick = () => {
     this.setState({ clickedMovieId: "" })
@@ -22,7 +32,7 @@ class App extends Component {
     return (
       <>
         {!this.state.clickedMovieId ?
-        <HomePage onMovieClick={this.onMovieClick}/> : <MovieSnapShot onHomeClick={this.onHomeClick}/>}
+        <HomePage onMovieClick={this.onMovieClick}/> : <MovieSnapShot singleMovie={this.state.singleMovie} trailer={this.state.trailer} onHomeClick={this.onHomeClick}/>}
       </>
     )
   }
