cd17caad065b6366fb939e70a64b9142bc27cedb
Elizabeth Hahn <elizhahn87@gmail.com>
2021-03-26 18:25:03 -0600

    Render trailer when movie poster is clicked

--- src/MovieSnapShot/MovieSnapShot.js
+++ src/MovieSnapShot/MovieSnapShot.js
@@ -4,16 +4,19 @@ import Footer from "../Footer/Footer";
 import MovieCard from "../MovieCard/MovieCard";
 import singleMovie from "../data/singleMovie.js";
 import "./MovieSnapShot.css";
+import trailer from "../data/trailer-data.js";
 
 class MovieSnapShot extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      singleMovie: singleMovie.movie
+      singleMovie: singleMovie.movie,
+      trailerToUse: trailer.videos[0]
     }
   }
 
   render() {
+    const trailerToPlay = `https://www.youtube.com/embed/${this.state.trailerToUse.key}`;
     const genres = this.state.singleMovie.genres.join(" | ");
     return (
       <div className="background-img" style={{backgroundImage: `url(${this.state.singleMovie.backdrop_path})`}}>
@@ -31,6 +34,7 @@ class MovieSnapShot extends Component {
             <p>{this.state.singleMovie.overview}</p>
             <p>{genres}</p>
             <p>runtime: {this.state.singleMovie.runtime}</p>
+            <iframe width="420 height" height="315" src={trailerToPlay}></iframe>
           </section>
         </main>
         <Footer />
