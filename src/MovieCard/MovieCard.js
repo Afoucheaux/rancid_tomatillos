e93c5c73d76458efaaecb0c51917645e5a46e277
Elizabeth Hahn <elizhahn87@gmail.com>
2021-03-27 18:09:02 -0600

    Add class style prop for movie card

--- src/MovieCard/MovieCard.js
+++ src/MovieCard/MovieCard.js
@@ -1,9 +1,9 @@
 import React from "react";
 import "./MovieCard.css"
 
-const MovieCard = ({id, posterImage, title, averageRating, releaseDate, handleMovieClick }) => {
+const MovieCard = ({id, posterImage, title, averageRating, releaseDate, handleMovieClick, classStyle }) => {
   return (
-    <article onClick={handleMovieClick} className="card-container" id={id}>
+    <article onClick={handleMovieClick} className={classStyle} id={id}>
       <img className= "card-image" src={posterImage} alt={`movie poster for ${title}`}/>
       <p className="card-text">{title}</p>
       <p className="card-text">Rating | {averageRating}</p>
