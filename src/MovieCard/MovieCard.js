8a9bc4d1bb1d3cc68e108fb09af97b8d6c6e1f92
Aaron Foucheaux <afoucheaux@gmail.com>
2021-03-28 11:44:29 -0500

    bug fix with single view image continued style update

--- src/MovieCard/MovieCard.js
+++ src/MovieCard/MovieCard.js
@@ -1,11 +1,11 @@
 import React from "react";
 import "./MovieCard.css"
 
-const MovieCard = ({id, posterImage, title, averageRating, releaseDate, handleMovieClick, classStyle }) => {
+const MovieCard = ({id, posterImage, title, averageRating, releaseDate, handleMovieClick, classStyle, imageStyle}) => {
   return (
     <article onClick={handleMovieClick} className={classStyle} id={id}>
-      <img className= "card-image" src={posterImage} alt={`movie poster for ${title}`}/>
-      <p className="card-text">{title}</p>
+      <img className= {imageStyle} src={posterImage} alt={`movie poster for ${title}`}/>
+      <p className="movie-title">{title}</p>
       <p className="card-text">Rating | {averageRating}</p>
       <p className="card-text">Released | {releaseDate}</p>
     </article>
