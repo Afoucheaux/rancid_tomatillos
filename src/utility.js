ca70b4f3836719c347e00b7947df4fb3e2b313f8
Elizabeth Hahn <elizhahn87@gmail.com>
2021-03-28 15:51:38 -0600

    Create single movie data file cleaner

--- src/utility.js
+++ src/utility.js
@@ -1,10 +1,10 @@
-export const trailerCleaner = (data) => {
-  console.log(data);
-  let trailer;
-  if(data) {
-    return false
-  } else {
-    trailer = `https://www.youtube.com/embed/${data.key}`;
-    return trailer;  
+export const singleMovieCleaner = (data) => {
+  let cleanedData = data.movie
+  if(data.movie.backdrop_path.includes("NoPhotoAvailable")) {
+    cleanedData = {...data.movie, backdrop_path: ""}
+  } 
+  if(!data.movie.runtime) {
+    cleanedData = {...cleanedData, runtime: "unavailable"};
   }
+  return cleanedData;
 }
