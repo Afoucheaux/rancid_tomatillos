a8d2a8a350bb2480e70ade8d3ad3c2f8a210dd1c
Elizabeth Hahn <elizhahn87@gmail.com>
2021-03-29 22:16:52 -0600

    Add Link to MovieCard component

--- src/MovieCard/MovieCard.js
+++ src/MovieCard/MovieCard.js
@@ -1,15 +1,16 @@
 import React from "react";
 import "./MovieCard.css";
 import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
-const MovieCard = ({id, posterImage, title, averageRating, releaseDate, handleMovieClick, classStyle, imageStyle}) => {
+const MovieCard = ({id, posterImage, title, averageRating, releaseDate, classStyle, imageStyle}) => {
   return (
-    <article onClick={handleMovieClick} className={classStyle} id={id}>
-      <img className= {imageStyle} src={posterImage} alt={`movie poster for ${title}`}/>
-      <p className="movie-title">{title}</p>
-      <p className="card-text">Rating | {averageRating}</p>
-      <p className="card-text">Released | {releaseDate}</p>
-    </article>
+      <Link to={`/${id}`} className={classStyle}>
+        <img className= {imageStyle} src={posterImage} alt={`movie poster for ${title}`}/>
+        <p className="movie-title">{title}</p>
+        <p className="card-text">Rating | {averageRating}</p>
+        <p className="card-text">Released | {releaseDate}</p>
+      </Link>
   )
 }
 
