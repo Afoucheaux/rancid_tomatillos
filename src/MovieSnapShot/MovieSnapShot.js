0a79e007dbc923fa40cb2e7d15c29f2f7ef2350c
Elizabeth Hahn <elizhahn87@gmail.com>
2021-03-29 22:19:32 -0600

    Convert movieSnapShot to class component that makes api call

--- src/MovieSnapShot/MovieSnapShot.js
+++ src/MovieSnapShot/MovieSnapShot.js
@@ -1,51 +1,82 @@
-import React from "react";
+import React, { Component } from "react";
 import Header from "../Header/Header";
 import Footer from "../Footer/Footer";
 import MovieCard from "../MovieCard/MovieCard";
 import "./MovieSnapShot.css";
 import PropTypes from "prop-types";
+import { getSingleMovieData } from "../apiCalls.js"
+import { singleMovieCleaner, trailerCleaner } from "../utility.js";
 
-    const MovieSnapShot = ({ singleMovie, trailer, onHomeClick, error}) => {
-    const trailerToPlay = `https://www.youtube.com/embed/${trailer.key}`;
-    const genres = singleMovie.genres.join(" | ");
+class MovieSnapShot extends Component {
+  constructor(props) {
+    super(props) 
+    this.state= {
+      clickedMovieId: "",
+      singleMovie: {},
+      trailer: {},
+      error: ""
+    }
+  }
+
+  componentDidMount() {
+    const id = this.props.id
+    console.log("test")
+    console.log(id);
+    getSingleMovieData(id)
+    .then(data => {
+      const [singleMovie, trailer] = data
+      const cleanedSingleMovie = singleMovieCleaner(singleMovie);  
+      const cleanedTrailer = trailerCleaner(trailer);
+      this.setState({clickedMovieId: id, singleMovie: cleanedSingleMovie, trailer: cleanedTrailer[0]})
+    })
+    .catch(error => this.setState({error: error.message}) )
+  }
+
+  render () {  
+   if(!this.state.clickedMovieId) {
+    return (<h1>Loading movie....</h1>)
+  } else {
+    const trailerToPlay = `https://www.youtube.com/embed/${this.state.trailer.key}`;
+    const genres = this.state.singleMovie.genres.join(" | ");
     const styles = {
-        background: singleMovie.backdrop_path ? `-webkit-linear-gradient(rgba(0,0,0, .3) 0%,rgba(0,0,0, 1) 100%), url(${singleMovie.backdrop_path}) no-repeat center center fixed` : null,
+        background: this.state.singleMovie.backdrop_path ? `-webkit-linear-gradient(rgba(0,0,0, .3) 0%,rgba(0,0,0, 1) 100%), url(${this.state.singleMovie.backdrop_path}) no-repeat center center fixed` : null,
         backgroundSize: "cover"
     }
-     return (
+    return (
       <div className="background-img" style={styles}>
-        <Header onHomeClick={onHomeClick}/>
+        <Header/>
         <main className="single-movie-container">
-          {error && <h1>There was an issue, please refresh and try again</h1>}
+          {this.state.error && <h1>There was an issue, please refresh and try again</h1>}
           <MovieCard
-            key={singleMovie.id}
-            id={singleMovie.id}
-            posterImage={singleMovie.poster_path}
-            title={singleMovie.title}
-            averageRating={singleMovie.average_rating.toFixed(1)}
-            releaseDate={singleMovie.release_date}
+            key={this.state.singleMovie.id}
+            id={this.state.singleMovie.id}
+            posterImage={this.state.singleMovie.poster_path}
+            title={this.state.singleMovie.title}
+            averageRating={this.state.singleMovie.average_rating.toFixed(1)}
+            releaseDate={this.state.singleMovie.release_date}
             classStyle={"card-container-single-movie"}
             imageStyle={"no-image-hover"}
           />
           <section className="extra-info-container">
-             <iframe className="extra-info-trailer" src={trailerToPlay} title="Movie Trailer" aria-label={`${singleMovie.title} trailer`}></iframe>
-            <p className="extra-info">{singleMovie.overview}</p>
+              <iframe className="extra-info-trailer" src={trailerToPlay} title="Movie Trailer" aria-label={`${this.state.singleMovie.title} trailer`}></iframe>
+            <p className="extra-info">{this.state.singleMovie.overview}</p>
             <p className="extra-info">{genres}</p>
-            <p className="extra-info">Runtime: {singleMovie.runtime} min</p>
+            <p className="extra-info">Runtime: {this.state.singleMovie.runtime} min</p>
           </section>
         </main>
         <Footer />
       </div>
     )
-  // }
+  }   
+  }
 }
-
-
+    
+    
 export default MovieSnapShot;
 
-MovieSnapShot.propTypes = {
-  singleMovie: PropTypes.object,
-  trailer: PropTypes.object,
-  onHomeClick: PropTypes.func,
-  error: PropTypes.string
-}
+// MovieSnapShot.propTypes = {
+//   singleMovie: PropTypes.object,
+//   trailer: PropTypes.object,
+//   onHomeClick: PropTypes.func,
+//   error: PropTypes.string
+// }
